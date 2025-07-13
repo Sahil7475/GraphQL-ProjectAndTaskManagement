2497f7666dd7fcf9350ccbd4f9fde2b4ba64d4f9
Sahil7475 <sahilj5645@gmail.com>
2025-07-14 01:38:22 +0530

    [fix] dbcontext and model

--- Models/Project.cs
+++ Models/Project.cs
@@ -1,10 +1,15 @@
-﻿namespace ProjectAndTaskManagement.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectAndTaskManagement.Models;
+
+public partial class Project
 {
-    public class Project
-    {
-        public int Id { get; set; }
-        public string Title { get; set; } = null!;
-        public string Description { get; set; } = null!;
-        public List<TaskItem> Tasks { get; set; } = new();
-    }
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public virtual ICollection<TaskItem> TaskItems { get; set; } = new List<TaskItem>();
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,11 +1,15 @@
-﻿namespace ProjectAndTaskManagement.Models
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = null!;
-        public string Role { get; set; } = "Member";
-        public List<TaskItem> AssignedTasks { get; set; } = new();
+﻿using System;
+using System.Collections.Generic;
 
-    }
+namespace ProjectAndTaskManagement.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Role { get; set; } = null!;
+
+    public virtual ICollection<TaskItem> TaskItems { get; set; } = new List<TaskItem>();
 }
